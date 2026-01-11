ae380c4328ac0d9922ca0996c85cb804055382d8
John Pert Millena <jmillena_21ur0307@psu.edu.ph>
2026-01-12 00:23:29 +0800

    inititated the backend setup

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express'; 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res)=>{
+    res.json({
+        system: "orBIT",
+        status: "ONLINE",
+        message: "I'm watching."
+    });
+});
+
+app.listen(PORT, ()=>{
+    console.log(`orBIT is running on http://localhost:${PORT}`);
+});
